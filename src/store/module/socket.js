5a3af748a83c9e4c76cdd5c42b94357604599e8c
zhangyongwang <765298337@qq.com>
2020-02-19 20:46:05 +0800

    close websocket

--- src/store/module/socket.js
+++ src/store/module/socket.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import * as config from '../../config'
 import httpCli from '../../api/request'
-import websocket from '../../api/websocket'
+// import websocket from '../../api/websocket'
 
 export default {
   state: {
@@ -14,25 +14,25 @@ export default {
   },
   mutations: {
       _watch(state,data){
-          websocket.watch(res => {
-              if (res) {
-                  let data = JSON.parse(res.data)
-                  console.log(data)
-                  if (data.code == '1' && data.message == '扫描作业') {
-                      state.scanSocketData = data.data
-                  }else if (data.code == '2' && data.message == '脱敏作业'){
-                      state.taskSocketData = data.data
-                  }
-              }
-          })
+          // websocket.watch(res => {
+          //     if (res) {
+          //         let data = JSON.parse(res.data)
+          //         console.log(data)
+          //         if (data.code == '1' && data.message == '扫描作业') {
+          //             state.scanSocketData = data.data
+          //         }else if (data.code == '2' && data.message == '脱敏作业'){
+          //             state.taskSocketData = data.data
+          //         }
+          //     }
+          // })
       }
   },
   actions: {
       _open({commit}){
           // websocket 实时监听任务状态
-          websocket.open(e => {
-              commit('_watch',e)
-          })
+          // websocket.open(e => {
+          //     commit('_watch',e)
+          // })
       }
   }
 }
